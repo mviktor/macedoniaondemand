422032f3fbc33eaee8dcb48b1b2dcbd4aac0f73c
Viktor Mladenovski <viktor.mladenovski@gmail.com>
2014-02-28 20:17:59 +0100

    24vesti: подршка за постари емисии

--- plugin.video.macedoniaondemand/addon.py
+++ plugin.video.macedoniaondemand/addon.py
@@ -237,8 +237,12 @@ def play24VestiVesti():
 	pDialog.close()
 	return True
 
-def create24VestiEmisiiListing():
-	url = 'http://24vesti.mk/video/emisii'
+def create24VestiEmisiiListing(urlpagenr):
+	if urlpagenr == None or urlpagenr == '':
+		url = 'http://24vesti.mk/video/emisii'
+	else:
+		url = 'http://24vesti.mk/video/emisii?page='+urlpagenr
+
 
 	req = urllib2.Request(url)
 	req.add_header('User-Agent', user_agent)
@@ -1006,13 +1010,19 @@ def PROCESS_PAGE(page,url='',name=''):
 		xbmcplugin.endOfDirectory(int(sys.argv[1]))
 
 	elif page == '24vesti_emisii':
-		listing = create24VestiEmisiiListing()
+		listing = create24VestiEmisiiListing(url)
 		for u,thumb,title in listing:
 			if title.__contains__('WIN-WIN'):
 				addLink(title, u, '24vesti_playvideo', thumb,'http://a1on.mk/wordpress/wp-content/uploads/2013/01/olivera-trajkovska.jpg')
 			else:
 				addLink(title, u, '24vesti_playvideo', thumb)
-		setView('files', 500)
+		if url == '' or url == None:
+			addDir('Претходно', '24vesti_emisii', '1', '')
+		else:
+			urlpage = int(url)+1
+			addDir('Претходно', '24vesti_emisii', str(urlpage), '')
+
+		setView('movies', 500)
 		xbmcplugin.endOfDirectory(int(sys.argv[1]))
 
 	elif page == '24vesti_videosodrzina':
